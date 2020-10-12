3ad76fd592e9f8eb9d7f5cf50fbbb7f9bab1e0f0
paveleveline <evelyn_lazau@yahoo.com>
2020-10-12 23:59:12 +0300

    Started creating navigation-layout

---
+++ resources/main.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    const menuItems = document.getElementsByClassName("menu-item");
+    console.log('menuItems', menuItems)
+
+    for (let i = 0; i < menuItems.length; i++) {
+        const menuItem = menuItems[i];
+        
+        menuItem.addEventListener('click', function() {
+            for(let j = 0; j < menuItems.length; j++) {
+                if(menuItems[j].classList.contains('selected')){
+                    menuItems[j].classList.remove("selected");
+                } else {
+                    continue;
+                }
+            }
+            menuItem.classList.add("selected");
+
+
+            const dataContent = menuItem.dataset.content;
+            console.log('dataContent', dataContent)
+            const contentElements = document.getElementsByClassName("content");
+            for(let k = 0; k < contentElements.length; k++) {
+                if(contentElements[k].classList.contains('hidden')) {
+                    continue;
+                } else {
+                    contentElements[k].classList.add("hidden");
+                }
+            }
+            document.getElementsByClassName(dataContent)[0].classList.remove("hidden");
+        
+            // const logo = document.getElementsByClassName('logo-image');
+            // To be continued
+        });
+    }
+});
